63311cc4e2049632fa68f2d7ae3d4bae3b52e82a
naiiiden <na4ko359@gmail.com>
2022-09-25 20:30:14 +0300

    mobile menu change aria label on open/close

--- script/script.js
+++ script/script.js
@@ -2,7 +2,9 @@ document.getElementById("hamburger").addEventListener("click", () => {
     document.getElementById("nav-ul").classList.toggle("show");
     if (document.getElementById("menu-icon").getAttribute("src") === "images/icon-hamburger.svg") {
         document.getElementById("menu-icon").setAttribute("src", "images/icon-close.svg");
+        document.querySelector("#hamburger").ariaLabel = "Close menu";
     } else {
         document.getElementById("menu-icon").setAttribute("src", "images/icon-hamburger.svg");
+        document.querySelector("#hamburger").ariaLabel = "Open menu";
     }
 });
